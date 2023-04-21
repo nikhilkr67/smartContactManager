29a0c08a19f6f06102785d1b6091efce797c5801
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-21 22:37:34 +0530

    Add files via upload

--- service/Impl/UserDetailsServiceImpl.java
+++ service/Impl/UserDetailsServiceImpl.java
@@ -1,1 +1,26 @@
+package com.smartcontact.service;
 
+import com.smartcontact.entities.User;
+import com.smartcontact.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //fetching user from Database
+        User user = userRepository.getUserByUserName(username);
+
+        if(user==null){
+            throw new UsernameNotFoundException("Could Not Found User !!");
+        }
+        CustomUserDetails customUserDetails= new CustomUserDetails(user);
+        return customUserDetails;
+    }
+}

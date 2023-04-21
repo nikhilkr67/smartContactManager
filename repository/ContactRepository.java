a6e7a40a8aca0c1c5ddf32ac99f4b772edb6fb45
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-21 22:33:13 +0530

    Add files via upload

---
+++ repository/ContactRepository.java
@@ -0,0 +1,16 @@
+package com.smartcontact.repository;
+
+import com.smartcontact.entities.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
+    @Query("from Contact as c where c.user.id=:userId")
+    public Page<Contact> findContactByUser(@Param("userId") int userId, Pageable pageable);
+
+}
